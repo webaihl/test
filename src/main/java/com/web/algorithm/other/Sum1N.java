499f674ba72da1bc21bc3303259fd119f414ca02
webaihl <webaihl@gmail.com>
2020-08-17 21:29:45 +0800

    1+...+n递归

---
+++ src/main/java/com/web/algorithm/other/Sum1N.java
@@ -0,0 +1,21 @@
+package com.web.algorithm.other;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @ClassName Sum1N.java
+ * @Description 1+...+N
+ * @createTime 2020年08月17日 19:50:00
+ */
+public class Sum1N {
+
+    public static int solution(int n, int s) {
+        boolean b = n > 0 && (s = solution(n - 1, s)) > 0;
+        return s + n;
+    }
+
+    public static void main(String[] arg) {
+        int s = solution(100, 0);
+        System.out.println(s);
+    }
+}
